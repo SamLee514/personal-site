806d616c41e4a82bbacf9906d18660ee2e6272b2
Samuel Lee <samlee514@Samuels-MacBook-Pro.local>
2021-08-26 18:29:10 -0500

    yes

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ const YELLOW = "#EBCB8B";
 const GREEN = "#A3BE8C";
 const PINK = "#B48EAD";
 const DAMP = "#D8DEE9";
+const GREY = "#4C566A";
 
 function isPage(item: Page | Folder): item is Page {
     return (item as Page).icon !== undefined;
@@ -23,7 +24,7 @@ async function getContent(): Promise<Map<string, (Page|Folder)>> {
         color: GREEN,
         innerHTML: await processMarkdown("public/content/test.md") 
     });
-    content.set("my work", {
+    content.set("my-work", {
         subDocs: new Map([
             ["projects", {
                 icon: "fas fa-tools",
@@ -76,16 +77,16 @@ function openPage(name: string, item: Page) {
     const tab = document.getElementById(tabId);
     if (tab === null) {
         document.getElementById('tabs')!.innerHTML += `
-            <button id="${tabId}">
+            <button id="${tabId}" name=${name}>
                 <i class="${item.icon}" style="color:${item.color}; width:1em"></i>
                 ${name}
-                <i class="fas fa-times fa-xs close-button" id="${closeId}" onclick="document.getElementById('${tabId}').remove();"></i>
+                <i class="fas fa-times fa-xs close-button" id="${closeId}"></i>
             </button> 
         `
     }
     const editor = document.getElementById("editor")! 
     editor.innerHTML = item.innerHTML;
-    editor.classList.add("active");
+    editor.classList.remove("inactive");
     makeExclusivelyActive(name);
 }
 
@@ -93,10 +94,11 @@ let currentlyActiveItemName: string;
 
 async function fillHTML(container: HTMLElement, items: Map<string, (Page|Folder)>, keyPrefix: string | null) {
     items.forEach((item, name) => {
-        const buttonId = `${name}-explorer`;
+        const processedName = keyPrefix ? keyPrefix + '/' + name : name;
+        const buttonId = `${processedName}-explorer`;
         if (isPage(item)) {
             container.innerHTML += `
-                <button id="${buttonId}" name="${keyPrefix ? keyPrefix + '/' + name : name}">
+                <button id="${buttonId}" name="${processedName}">
                     <i class="${item.icon}" style="color:${item.color}; width:1em"></i>
                     ${name}
                 </button>
@@ -105,13 +107,13 @@ async function fillHTML(container: HTMLElement, items: Map<string, (Page|Folder)
             const iconId = `${name}-icon`;
             const accordionId = `${name}-accordion`;
             container.innerHTML += `
-                <button id="${buttonId}" name="${name}">
+                <button id="${buttonId}" name="${processedName}">
                     <i id="${iconId}" class="fas fa-chevron-right" style="color:${DAMP}; width:1em"></i>
                     ${name}
                 </button>
                 <div id="${accordionId}" class="folder-contents" style="max-height:0px;overflow:hidden;"></div>
             `
-            fillHTML(document.getElementById(`${accordionId}`)!, item.subDocs, name);
+            fillHTML(document.getElementById(`${accordionId}`)!, item.subDocs, processedName);
         }
     });
 }
@@ -140,12 +142,49 @@ async function main() {
         } else {
             return;
         }
-        const nameComponents = (element as HTMLElement).getAttribute("name")!.split('/');
-        const item = deepAccess(content, nameComponents)!;
+        const name = (element as HTMLElement).getAttribute("name")!;
+        const item = deepAccess(content, name.split('/'))!;
         if (isPage(item)) {
-            openPage(nameComponents[nameComponents.length - 1], item);
+            openPage(name, item);
         } else {
-            toggleOpenFolder(nameComponents[nameComponents.length - 1]);
+            toggleOpenFolder(name);
+        }
+    });
+
+    document.getElementById("tabs")!.addEventListener("click", function (this: HTMLElement, e: MouseEvent) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        let element;
+        if ((e.target! as HTMLElement).tagName === "BUTTON") {
+            element = e.target!;
+        } else if ((e.target! as HTMLElement).parentElement!.tagName === "BUTTON") {
+            element = (e.target! as HTMLElement).parentElement!;
+            if ((e.target! as HTMLElement).classList.contains("close-button")) {
+                console.log("HERE!", element, document.getElementById("tabs")!.innerHTML)
+                if (element.nextElementSibling) {
+                    const name = (element.nextElementSibling as HTMLElement).getAttribute("name")!;
+                    openPage(name, (deepAccess(content, name.split('/'))! as Page));
+                } else if (element.previousElementSibling) {
+                    const name = (element.previousElementSibling as HTMLElement).getAttribute("name")!;
+                    openPage(name, (deepAccess(content, name.split('/'))! as Page));;
+                } else {
+                    document.getElementById(`${currentlyActiveItemName}-explorer`)!.classList.remove("active");
+                    const editor = document.getElementById("editor")!;
+                    editor.innerHTML = `<i class="fas fa-smile-wink fa-10x" style="color:${GREY};"></i>`;
+                    editor.classList.add("inactive");
+                }
+                (e.target! as HTMLElement).parentElement!.remove();
+                return;
+            }
+        } else {
+            return;
+        }
+        const name = (element as HTMLElement).getAttribute("name")!;
+        const item = deepAccess(content, name.split('/'))!;
+        if (isPage(item)) {
+            openPage(name, item);
+        } else {
+            toggleOpenFolder(name);
         }
     });
 }
