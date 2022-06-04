b7fa3821bd876bc8b412c2a437ccd7c879b04a2c
Samuel Lee <sam@samlee.dev>
2022-06-04 18:20:57 -0400

    update main page and formatting

---
+++ src/styles/deleteMe.js
@@ -0,0 +1,15 @@
+/*
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+*/
+
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function (accounts) {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+};
