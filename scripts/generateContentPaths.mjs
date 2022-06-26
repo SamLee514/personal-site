fd5a94a5843894a5928676668ebf9b65e1e83136
Samuel Lee <sam@samlee.dev>
2022-06-26 15:37:35 -0400

    add generate content tree script

---
+++ scripts/generateContentPaths.mjs
@@ -0,0 +1,19 @@
+import * as fs from "fs";
+import path from "path";
+
+function traverseDir(dir, tree) {
+  fs.readdirSync(dir).forEach((file) => {
+    let fullPath = path.join(dir, file);
+    const name = path.parse(file).name;
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      tree[name] = {};
+      traverseDir(fullPath, tree[name]);
+    } else {
+      tree[name] = fullPath;
+    }
+  });
+}
+
+const tree = {};
+traverseDir("content", tree);
+fs.writeFileSync("content_tree.json", JSON.stringify(tree, null, 2));
