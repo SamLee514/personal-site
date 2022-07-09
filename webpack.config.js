8fd8ace23bdc2c9af66c5d8b1414474005ba5ed3
Samuel Lee <sam@samlee.dev>
2022-07-08 22:52:17 -0400

    bye devdependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,9 @@
 const path = require("path");
-const webpack = require("webpack");
+
 module.exports = {
-  entry: "./src/app.ts",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(
-        process.env.NODE_ENV || "development"
-      ),
-    }),
-  ],
+  entry: "./src/index.js",
   output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
