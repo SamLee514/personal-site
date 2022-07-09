7f790166b7b966d10953e16c804a736f0e92b2a6
Samuel Lee <sam@samlee.dev>
2022-07-08 22:59:02 -0400

    come back me webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require("path");
-
+const webpack = require("webpack");
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/app.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development"
+      ),
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
+    path: path.resolve(__dirname, "public"),
   },
 };
